0fb965901f3b3a519164fae1a17638d628ff7a97
Evgeny P <evgeny-p@users.noreply.github.com>
2016-07-09 19:19:35 +0600

    [emacs] Set work indentation style

--- dot/emacs/.emacs.d/settings/modes/cpp.el
+++ dot/emacs/.emacs.d/settings/modes/cpp.el
@@ -1,9 +1,22 @@
+(c-add-style "my-style"
+             '("stroustrup"
+               (indent-tabs-mode . t)          ; use tabs rather than spaces
+               (c-basic-offset . 4)            ; indent by four spaces
+               (tab-width . 4)
+               (c-offsets-alist . ((inline-open . 0)  ; custom indentation rules
+                                   (brace-list-open . 0)
+                                   (innamespace . 0)
+                                   (statement-case-open . +)
+                                   (arglist-cont-nonempty . +)
+                                   (member-init-intro . 0)))))
+
 (defun my-c++-mode-hook ()
+  (c-set-style "my-style")
   (show-paren-mode 1)
   (define-key c-mode-base-map [f4] 'ff-find-related-file)
 
   ; fill column indicator
   (fci-mode 1)
-  (setq fill-column 80))
+  (setq fill-column 100))
 
 (add-hook 'c++-mode-hook 'my-c++-mode-hook)
