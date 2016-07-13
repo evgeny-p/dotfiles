0f1037de59c0285db8b719affe131705e460e4bb
Evgeny P <evgeny-p@users.noreply.github.com>
2016-07-13 22:00:20 +0600

    [emacs] Set fill-column-indicator for c++-mode

--- dot/emacs/.emacs.d/settings/modes/cpp.el
+++ dot/emacs/.emacs.d/settings/modes/cpp.el
@@ -1,5 +1,9 @@
 (defun my-c++-mode-hook ()
   (show-paren-mode 1)
-  (define-key c-mode-base-map [f4] 'ff-find-related-file))
+  (define-key c-mode-base-map [f4] 'ff-find-related-file)
+
+  ; fill column indicator
+  (fci-mode 1)
+  (setq fill-column 80))
 
 (add-hook 'c++-mode-hook 'my-c++-mode-hook)
