3f7f1dc3b70d5a2e9e69dbf457afb515adcf7e35
Evgeny P <evgeny-p@users.noreply.github.com>
2016-07-22 14:11:09 +0600

    [emacs] Switch to light theme

--- dot/emacs/.emacs.d/settings/appearance.el
+++ dot/emacs/.emacs.d/settings/appearance.el
@@ -6,7 +6,9 @@
 
 ;; Monokai
 ; Ignore "Wrong type argument: stringp, highlight"
-(ignore-errors (load-theme 'monokai t))
+;(ignore-errors (load-theme 'monokai t))
+(load-theme 'greymatters t)
+(set-cursor-color "#333")
 
 (setq neo-theme 'ascii)
 
@@ -16,6 +18,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(hl-line ((t (:background "#444")))))
-
-
+ ;'(hl-line ((t (:background "#444")))))
+ '(hl-line ((t (:background "#eee")))))
