892b62b2e5dc9d5d640ae36b2d1912c19e87e50a
Evgeny P <evgeny-p@users.noreply.github.com>
2017-01-01 20:35:00 +0700

    [xkb] Remove useless timeout

--- scripts/xkbcomp-load.sh
+++ scripts/xkbcomp-load.sh
@@ -1,5 +1,1 @@
-# Wait DE and override its setttings
-# http://superuser.com/questions/644521/linux-mint-mate-use-xkbcomp-to-load-a-keyboard-layout-on-startup
-sleep 5
-
 xkbcomp ~/.config/xkb/personal $DISPLAY
